1503da210459a882c1f76ca681e9701800b6a04c
Trịnh Duy Kiên <63652606+kientrinh24-05@users.noreply.github.com>
2022-08-09 15:33:47 +0700

    DS

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../core/authentication.service';
+import { first } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { MustMatch } from 'src/app/core/helper/must-match.validator';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  frmLogin:FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error='';
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) {
+      // redirect to home if already logged in
+      if (this.authenticationService.userValue) {
+        this.router.navigate(['/auth/login']);
+      }
+    }
+    
+    ngOnInit(): void {
+      this.frmLogin = this.formBuilder.group({
+        username: ['', [Validators.required]],
+        password: [['', Validators.required]],
+        remember: [''],
+      },{
+        // validator: MustMatch('username', 'NhapLaiMatKhau')
+      });
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+    
+    get f() {
+      return this.frmLogin.controls;
+    }
+    
+    onSubmit(){
+      this.submitted = true;
+      
+      // stop here if form is invalid
+      if (this.frmLogin.invalid) {
+        return;
+      }
+      this.loading = true;
+      this.authenticationService.login(this.f.username.value, this.f.password.value).pipe(first()).subscribe((data) => {
+        this.router.navigate([this.returnUrl]);
+      },(error) => {
+        alert("Đăng nhập thất bại");
+        this.loading = false;   
+      });
+    }
+  }
+  
